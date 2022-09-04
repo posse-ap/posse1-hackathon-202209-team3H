3f8401f85beffd1f551a6324317572c1e0c83e7f
Ayumi Fujii <ayu0223g@gmail.com>
2022-09-04 15:10:39 +0900

    ハンバーガーメニュー完了

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -4,8 +4,10 @@
   const header = document.getElementById('js-header');
   const headerHamburger = document.getElementById('js-headerHamburger');
   const main = document.getElementById('js-main');
+  const footer = document.getElementById('js-footer');
   headerHamburger.addEventListener('click', () => {
     header.classList.toggle('is-open');
     main.classList.toggle('hide');
+    footer.classList.toggle('is-changed');
   });
 }
