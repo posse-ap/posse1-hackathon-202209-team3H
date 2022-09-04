c2d886e0366e9a2cd630a5b7b35b0a5e03a15d81
Ayumi Fujii <ayu0223g@gmail.com>
2022-09-04 12:27:23 +0900

    ハンバーガーあとちょっと！

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -3,8 +3,9 @@
   // ハンバーガーメニュー
   const header = document.getElementById('js-header');
   const headerHamburger = document.getElementById('js-headerHamburger');
+  const main = document.getElementById('js-main');
   headerHamburger.addEventListener('click', () => {
-    header.classList.toggle('is-open')
-  })
-
+    header.classList.toggle('is-open');
+    main.classList.toggle('hide');
+  });
 }
